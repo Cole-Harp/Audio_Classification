8ea1e90ac32062e066ea210783f44f9cd91af00a
James <james.oreilly@mnsu.edu>
2023-11-26 14:20:36 -0600

    James- chord and file saving

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
-import pypiano
+"""
+SCAMP Example: Hello World
 
+Plays a C major arpeggio.
+"""
+# import the scamp namespace
+from scamp import *
 def main():
-    print("Hello World")
+    s = Session()
+    playback_settings.recording_file_path = "test.wav"
+    violin = s.new_part("Violin")
+    violin.play_chord([60, 64, 67, 72], 1, 1.0)
+
 
 if __name__ == "__main__":
     main()

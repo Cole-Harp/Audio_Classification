6c6157cdc375155f886de9f59f75d082f2475276
Cole-Harp <78562481+Cole-Harp@users.noreply.github.com>
2023-11-27 16:12:11 -0600

    vars

--- Chord_Ident/vars.py
+++ Chord_Ident/vars.py
@@ -1,7 +1,7 @@
-instruments = ["Piano", "Violin", "Organ", "Oboe", "Guitar"]
+instruments = ["Cello","Clarinet","Flute","Piano", "Violin", "Organ", "Oboe", "Guitar"]
 
 chords = [
-    ["C", "E", "G"],  # C major
+     ["C", "E", "G"],  # C major
     ["A", "C", "E"],  # A minor
     ["G", "B,3", "D"],  # G major
     ["F", "A", "C,5"],  # F major
@@ -11,12 +11,12 @@ chords = [
     ["C,5", "Eb", "G"],  # C minor
     ["A,2", "C#", "E"],  # A major
     ["G", "Bb", "D,6"],  # G minor
-    ["F,3", "Ab", "C"],  # F minor
+    ["F,3", "Ab", "C"],  # Fminor
     ["D", "F", "A"],  # D minor
     ["E", "G", "B,2"],  # E minor
     ["B", "D", "F#"],  # B minor
     ["C,5", "E", "G", "B"],  # C major 7th
-    ["A6", "C", "E", "G"],  # A minor 7th
+    ["A,6", "C", "E", "G"],  # A minor 7th
     ["G", "B,5", "D", "F"],  # G dominant 7th
     ["F", "A", "C", "E"],  # F major 7th
     ["D", "F#", "A,3", "C"],  # D minor 7th
@@ -40,6 +40,41 @@ chords = [
     ["G#", "D#"],  # G#5
     ["A", "E"],  # A5
     ["A#", "F"],  # A#5
-    ["B", "F#"]  # B7
+    ["B", "F#"],  # B7
+    ["C,2", "E,2", "G,2"],  # C major in 2nd octave
+    ["A,3", "C,3", "E,3"],  # A minor in 3rd octave
+    ["G,2", "B,2", "D,2"],  # G major in 2nd octave
+    ["F,2", "A,2", "C,3"],  # F major in 2nd and 3rd octaves
+    ["D,5", "F#", "A,5"],  # D major in 5th octave
+    ["E,3", "G#", "B,3"],  # E major in 3rd octave
+    ["B,2", "D#", "F#"],  # B major in 2nd octave
+    ["C,3", "E3", "G,3"],  # C minor in 3rd octave
+    ["A,5", "C#", "E,5"],  # A major in 5th octave
+    ["G,6", "B6", "D,7"],  # G minor in 6th and 7th octaves
+    ["F,5", "Ab5", "C,6"],  # F minor in 5th and 6th octaves
+    ["D,3", "F,3", "A,3"],  # D minor in 3rd octave
+    ["E,2", "G,2", "B,2"],  # E minor in 2nd octave
+    ["B,5", "D,5", "F#"],  # B minor in 5th octave
+    ["C,6", "E,6", "G,6", "B,6"],  # C major 7th in 6th octave
+    ["A,2", "C,2", "E,2", "G,2"],  # A minor 7th in 2nd octave
+    ["G,3", "B,3", "D,3", "F,3"],  # G dominant 7th in 3rd octave
+    ["F,6", "A,6", "C,7", "E,7"],  # F major 7th in 6th and 7th octaves
+    ["D,2", "F#", "A,2", "C,2"],  # D minor 7th in 2nd octave
+    ["E,5", "G#", "B,5", "D#"],  # E major 7th in 5th octave
+    ["B,6", "D#", "F#", "A#"],  # B major 7th in 6th octave
+    ["C,3", "E,3", "G,3", "B3"],  # C7 in 3rd octave
+    ["A,1", "C#", "E,1", "G,1"],  # A7 in 1st octave
+    ["G,5", "B,5", "D,5", "F#"],  # Gmaj7 in 5th octave
+    ["F,1", "A,1", "C,1", "E,1"],  # F7 in 1st octave
+    ["D,1", "F#", "A,1", "C#"],  # Dmaj7 in 1st octave
+    ["E,6", "G#", "B,6", "D,6"],  # E7 in 6th octave
+    ["B,3", "D#", "F#", "A,3"],  # B7 in 3rd octave
+    ["C,7", "G,7"],  # C5 in 7th octave
+    ["C#", "G#"],  # C#5 in 6th octave
+    ["D,7", "A,7"],  # D5 in 7th octave
+    ["D#", "A#"],  # D#5 in 6th octave
+    ["E,7", "B,7"],  # E5 in 7th octave
+    ["F,2", "C,2"],  # F5 in 2nd octave
+
 ]
 

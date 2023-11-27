d00eee6081157056f88c815ba89b80a997470d7a
Cole-Harp <78562481+Cole-Harp@users.noreply.github.com>
2023-11-27 02:37:28 -0600

    Best Looking Data

--- Chord_Ident/plots.py
+++ Chord_Ident/plots.py
@@ -12,49 +12,42 @@ from scipy.signal import spectrogram, find_peaks
 df = ""
 
 
+def plot(time, audio, N, freq, fft, fs):
+    # Plotting signal in time domain
 
+    fig, axes = plt.subplots(3, 2, figsize=(12, 8))
 
-def plot_freq_domain(y_freq, signal_f_onesided):
-    # Plotting signal in time and frequency domains
-    fig, axes = plt.subplots(1, 2, figsize=(12, 7))
+    axes[0, 0].plot(time, audio)
+    axes[0, 0].set_title("Sound Wave in Time Domain (No Zoom)")
+    axes[0, 0].set(xlabel='Time [sec]')
 
-    axes[0].plot(y_freq, signal_f_onesided)
-    axes[0].set_title("Sound Wave in Frequency Domain (No Zoom)")
-    axes[0].set(xlabel='Frequency [Hz]')
+    axes[0, 1].plot(time[(N // 2):(N // 2 + 480)], audio[(N // 2):(N // 2 + 480)])
+    axes[0, 1].set_title("Sound Wave in Time Domain (Zoomed)")
+    axes[0, 1].set(xlabel='Time [sec]')
 
-    axes[1].plot(y_freq[:5000], signal_f_onesided[:5000])
-    axes[1].set_title("Sound Wave in Frequency Domain (Zoomed)")
-    axes[1].set(xlabel='Frequency [Hz]')
-    fig.tight_layout()
-    plt.show()
+    # Plotting signal in freq domain
 
+    axes[1, 0].plot(freq, fft)
+    axes[1, 0].set_title("Sound Wave in Frequency Domain (No Zoom)")
+    axes[1, 0].set(xlabel='Frequency [Hz]')
 
+    axes[1, 1].plot(freq[:5000], fft[:5000])
+    axes[1, 1].set_title("Sound Wave in Frequency Domain (Zoomed)")
+    axes[1, 1].set(xlabel='Frequency [Hz]')
 
+    # Plotting signal in spectogram
 
-def plot_time_domain(time, audio, N):
-    fig, axes = plt.subplots(1, 2, figsize=(12, 7))
-    axes[0].plot(time, audio)
-    axes[0].set_title("Sound Wave in Time Domain (No Zoom)")
-    axes[0].set(xlabel='Time [sec]')
-
-    axes[1].plot(time[(N // 2):(N // 2 + 480)], audio[(N // 2):(N // 2 + 480)])
-    axes[1].set_title("Sound Wave in Time Domain (Zoomed)")
-    axes[1].set(xlabel='Time [sec]')
-    fig.tight_layout()
-    plt.show()
-
-
-def graph_spectogram(fs, audio):
     f, t, Sxx = spectrogram(audio, fs, nperseg=10000, nfft=50000)
 
     # Plots
-    fig, axes = plt.subplots(1, 2, figsize=(12, 5))
-    axes[0].pcolormesh(t, f, np.log(Sxx), cmap="jet")
-    axes[0].set_title("Spectogram (No Zoom)")
-    axes[0].set(xlabel='Time [sec]', ylabel='Frequency [Hz]')
-    axes[1].pcolormesh(t, f[:1500], np.log(Sxx)[:1500, :], cmap="jet")
-    axes[1].set_title("Spectogram (Zoomed)")
-    axes[1].set(xlabel='Time [sec]', ylabel='Frequency [Hz]')
+
+    axes[2, 0].pcolormesh(t, f, np.log(Sxx), cmap="jet")
+    axes[2, 0].set_title("Spectogram (No Zoom)")
+    axes[2, 0].set(xlabel='Time [sec]', ylabel='Frequency [Hz]')
+    axes[2, 1].pcolormesh(t, f[:1500], np.log(Sxx)[:1500, :], cmap="jet")
+    axes[2, 1].set_title("Spectogram (Zoomed)")
+    axes[2, 1].set(xlabel='Time [sec]', ylabel='Frequency [Hz]')
+    fig.tight_layout()
     plt.show()
 
 
@@ -66,15 +59,7 @@ def plots(index):
     freq = df.at[index, 'freq']
     time = np.linspace(0., N / fs, N)
 
-    plot_time_domain(time, audio, N)
-
-    plot_freq_domain(freq, fft)
-
-    graph_spectogram(fs, audio)
-
-
-
-
+    plot(time, audio, N, freq, fft, fs)
 
 
 if __name__ == "__main__":

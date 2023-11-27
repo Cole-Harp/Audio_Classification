33a0ebce922156a508424be8a579c51f0ad7e110
Cole-Harp <78562481+Cole-Harp@users.noreply.github.com>
2023-11-26 20:46:07 -0600

    main

--- Chord_Ident/main.py
+++ Chord_Ident/main.py
@@ -1,3 +1,16 @@
+import numpy as np
+import pandas as pd
 
+from vars import instruments, chords
+
+# [Instrument , Chords(A,G,F),
 def gen_data():
-    data_table = [][]
+
+    data_table = []
+    for i in range(0,len(instruments)):
+        for j in range(0,len(chords)):
+            data_table.append([instruments[i],chords[j]])
+    df = pd.DataFrame(data=data_table)
+    return df
+
+print(gen_data())
--- Chord_Ident/vars.py
+++ Chord_Ident/vars.py
@@ -1,45 +1,45 @@
 instruments = ["Piano", "Violin", "Organ", "Oboe", "Guitar"]
 
 chords = [
-    ["C", "E", "G"],  # C major
-    ["A", "C", "E"],  # A minor
-    ["G", "B", "D"],  # G major
-    ["F", "A", "C"],  # F major
-    ["D", "F#", "A"], # D major
-    ["E", "G#", "B"], # E major
-    ["B", "D#", "F#"],# B major
-    ["C", "Eb", "G"], # C minor
-    ["A", "C#", "E"], # A major
-    ["G", "Bb", "D"], # G minor
-    ["F", "Ab", "C"], # F minor
-    ["D", "F", "A"],  # D minor
-    ["E", "G", "B"],  # E minor
-    ["B", "D", "F#"], # B minor
-    ["C", "E", "G", "B"],  # C major 7th
-    ["A", "C", "E", "G"],  # A minor 7th
-    ["G", "B", "D", "F"],  # G dominant 7th
-    ["F", "A", "C", "E"],  # F major 7th
-    ["D", "F#", "A", "C"], # D minor 7th
-    ["E", "G#", "B", "D#"],# E major 7th
-    ["B", "D#", "F#", "A#"],# B major 7th
-    ["C", "E", "G", "Bb"],  # C7
-    ["A", "C#", "E", "G"],  # A7
-    ["G", "B", "D", "F#"],  # Gmaj7
-    ["F", "A", "C", "Eb"],  # F7
-    ["D", "F#", "A", "C#"], # Dmaj7
-    ["E", "G#", "B", "D"],  # E7
-    ["B", "D#", "F#", "A"],
-    ["C", "G"],  # C5
-    ["C#", "G#"],  # C#5
-    ["D", "A"],  # D5
-    ["D#", "A#"],  # D#5
-    ["E", "B"],  # E5
-    ["F", "C"],  # F5
-    ["F#", "C#"],  # F#5
-    ["G", "D"],  # G5
-    ["G#", "D#"],  # G#5
-    ["A", "E"],  # A5
-    ["A#", "F"],  # A#5
-    ["B", "F#"] # B7
+    ("C", "E", "G"),  # C major
+    ("A", "C", "E"),  # A minor
+    ("G", "B,3", "D"),  # G major
+    ("F", "A", "C,5"),  # F major
+    ("D,6", "F#", "A"),  # D major
+    ("E", "G#", "B"),  # E major
+    ("B", "D#", "F#"),  # B major
+    ("C,5", "Eb", "G"),  # C minor
+    ("A,2", "C#", "E"),  # A major
+    ("G", "Bb", "D,6"),  # G minor
+    ("F,3", "Ab", "C"),  # F minor
+    ("D", "F", "A"),  # D minor
+    ("E", "G", "B,2"),  # E minor
+    ("B", "D", "F#"),  # B minor
+    ("C,5", "E", "G", "B"),  # C major 7th
+    ("A6", "C", "E", "G"),  # A minor 7th
+    ("G", "B,5", "D", "F"),  # G dominant 7th
+    ("F", "A", "C", "E"),  # F major 7th
+    ("D", "F#", "A,3", "C"),  # D minor 7th
+    ("E", "G#", "B", "D#"),  # E major 7th
+    ("B", "D#", "F#", "A#"),  # B major 7th
+    ("C", "E", "G", "Bb"),  # C7
+    ("A", "C#", "E", "G"),  # A7
+    ("G", "B", "D,7", "F#"),  # Gmaj7
+    ("F", "A", "C", "Eb"),  # F7
+    ("D", "F#", "A", "C#"),  # Dmaj7
+    ("E", "G#", "B", "D"),  # E7
+    ("B", "D#", "F#", "A"),
+    ("C", "G"),  # C5
+    ("C#", "G#"),  # C#5
+    ("D", "A"),  # D5
+    ("D#", "A#"),  # D#5
+    ("E", "B"),  # E5
+    ("F", "C"),  # F5
+    ("F#", "C#"),  # F#5
+    ("G", "D"),  # G5
+    ("G#", "D#"),  # G#5
+    ("A", "E"),  # A5
+    ("A#", "F"),  # A#5
+    ("B", "F#")  # B7
 ]
 

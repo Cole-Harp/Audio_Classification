bb7d6835dddebbeba7c34995d4fbe17b9daf5576
James <james.oreilly@mnsu.edu>
2023-11-27 03:34:19 -0600

    cleaned code

--- Chord_Ident/sk_models.py
+++ Chord_Ident/sk_models.py
@@ -1,18 +1,19 @@
-import pandas as pd
+from pandas import read_pickle, DataFrame
 from sklearn.preprocessing import LabelEncoder
 from plots import Plotting
 df = None
-preprocessingsteps = None
+preprocessingsteps = []
 def main():
-    df = pd.read_pickle("wavs/processed_data.pickle")
-    plot = Plotting(df)
-    plot.plots(2)
-
-
-    #le = LabelEncoder()
-
+    df = read_pickle("wavs/processed_data.pickle")
+    le = LabelEncoder()
+    df['instrument']= le.fit_transform(df['instrument'])
+    preprocessingsteps.append(le)
+    lengths = []
+    for index in range(len(df)):
+        lengths.append(len(df.at[index, 'peaks']))
+    print(lengths)
     print(df.dtypes)
-
+    print(df.head)
 
 if __name__ == "__main__":
     main()

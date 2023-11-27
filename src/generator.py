2aae6d35096999e1b1995b5571222a8ac18a89a5
James <james.oreilly@mnsu.edu>
2023-11-26 19:38:22 -0600

    WIP sepperator

--- src/generator.py
+++ src/generator.py
@@ -1,6 +1,5 @@
 from scamp import *
 
-
 def note_to_midi(note, octave=4):
     # Define the note values
     note_values = {
@@ -14,30 +13,42 @@ def note_to_midi(note, octave=4):
 
     return midi_number
 
-
-def gen_wav(file_name, chord_list, instrument, duration=1.0):
-    midi_notes = []
-
-    s = Session()
-
-    playback_settings.recording_file_path = file_name
-
-    violin = s.new_part(instrument)
-    for notes in chord_list:
-        for note in notes:
-            midi_notes.append(note_to_midi(note))
-
-        violin.play_chord(midi_notes, 1, duration)
-        midi_notes = []
-
-    s.kill()
+def note_arg(arg):
+    note = arg.split(',')
+    if (len(note)<1 or len(note)>2):
+        raise TypeError(f"not a valid note: {arg}\t Length: {len(note)}")
+    elif (len(note)>1):
+        return note_to_midi(note[0],note[1])
+    else:
+        return note_to_midi(note[0], 4)
+class gen_Wav:
+    def __init__(self, file_name):
+        self.filename = file_name
+        playback_settings.recording_file_path = self.filename
+        self.s = Session()
+        self.chordCount = 0
+        self.duration = 2
+    def end_session(self):
+        self.s.kill()
+    def __del__(self):
+        self.end_session()
+    def gen_chord(self, note_list, instrument,):
+        inst = self.s.new_part(instrument)
+        inst.play_chord(note_list, .9, self.duration)
+        self.chordCount += 1
+    def gen_multi_chords(self, chord_list, instrument):
+        for note_list in chord_list:
+            self.gen_chord(note_list, instrument)
 
 
 import wave
 import audioop
 
 
-def split_wav(file_name, num_chords, chord_duration=1.0):
+def split_wav_from_gen(gen_wav):
+    #gen_wav.duration
+    split_wav(gen_wav.filename,gen_wav.chordCount,gen_wav.duration)
+def split_wav(file_name, num_chords, chord_duration):
     with wave.open(file_name, 'rb') as w:
         params = w.getparams()
         framerate = w.getframerate()
@@ -45,11 +56,12 @@ def split_wav(file_name, num_chords, chord_duration=1.0):
         audio = w.readframes(nframes)
 
     # Calculate the number of frames per chord
+    frameStart = int(framerate*1)
     frames_per_chord = int(framerate * chord_duration)
 
     # Split the audio into chunks and save as separate files
     for i in range(num_chords):
-        start = i * frames_per_chord
+        start = frameStart+(i * frames_per_chord)
         end = start + frames_per_chord
         chunk = audio[start:end]
 
--- src/main.py
+++ src/main.py
@@ -1,19 +1,15 @@
-"""
-SCAMP Example: Hello World
+import argparse
+import time
 
-Plays a C major arpeggio.
-"""
-# import the scamp namespace
 from feature_extraction import Data
-from src.wav_parser import fft
-from generator import gen_wav as GW, split_wav as SW
-def main():
-    notes = [["C", 'E', 'G', 'F'],['A', 'C', 'G']]
-    #GW("test2.wav", notes, "Piano", duration=2)
-    #SW('test2.wav', len(notes), 8)
+from generator import gen_Wav, note_arg, split_wav_from_gen
+
+
+def main2():
+
     #fft('test2_chord_1.wav')
-    d = Data('test2_chord_1.wav')
-    d2 = Data('test2_chord_2.wav')
+    d = Data('test_chord_1.wav')
+    d2 = Data('test_chord_2.wav')
     d.plot()
     d2.plot()
     d.gen_fft()
@@ -21,13 +17,32 @@ def main():
     d.plot_fft()
     d2.plot_fft()
 
-def main(args):
+
+
+def main(raw_args):
     parser = argparse.ArgumentParser(description='generate wav from notes.')
-    parser.add_argument('-o','--output',type=str, default="output.wav", required=False)
-    parser.add_argument('-n','--notes',action="extend", type=note, nargs='+', required=True)
-    temp = parser.parse_args(args)
-    print(temp)
-    #GW([CIM("F",2), CIM('E'), CIM("G")], "Piano")
+    parser.add_argument('-o','--outwav',action='store',type=str, default="output.wav", required=True)
+    parser.add_argument('-n','--notes',action="extend", type=note_arg, nargs='+', required=False)
+    args = parser.parse_args(raw_args)
+
+    GW = gen_Wav(args.outwav)
+    notes = [["C", 'E', 'G', 'F'], ['A', 'C', 'G']]
+    notes_midi =[]
+    for i in range(0, len(notes)):
+        notes_midi.append([])
+        for j in range(0,len(notes[i])):
+            notes_midi[i].append(note_arg(notes[i][j]))
+
+    GW.gen_multi_chords(notes_midi, "Piano")
+    GW.gen_multi_chords(notes_midi, "Violin")
+    GW.gen_multi_chords(notes_midi, "Organ")
+    time.sleep(10)
+    d = Data('test.wav')
+    d.plot()
+    split_wav_from_gen(GW)
+
+
+
 
 if __name__ == "__main__":
-    main()
+    main(["-o","test.wav"])
